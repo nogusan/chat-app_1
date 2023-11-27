57383f07d7591ea343c66fb8778ca182620f4a9a
nogusan <standing.nogusan@gmail.com>
2023-11-27 21:59:36 +0900

    room_user tableの作成

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,5 @@ Rails.application.routes.draw do
   devise_for :users
   root to:"messeges#index"
   resources :users, only: [:edit, :update]
-  resources :rooms, only: [:new :create]
+  resources :rooms, only: [:new, :create]
 end
