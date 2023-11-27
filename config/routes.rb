fb6737169d6663c36a0493ff13b47444096cbebe
nogusan <standing.nogusan@gmail.com>
2023-11-27 22:32:31 +0900

    画面遷移の実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
 
   devise_for :users
-  root to:"messeges#index"
+  root to:"rooms#index"
   resources :users, only: [:edit, :update]
   resources :rooms, only: [:new, :create]
 end
