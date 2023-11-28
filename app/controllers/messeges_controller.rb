eaa3e9af4eaa8b634b37bd34f7b1b02333a7f8a7
nogusan <standing.nogusan@gmail.com>
2023-11-28 22:03:43 +0900

    メッセージ送信機能の実装

--- app/controllers/messeges_controller.rb
+++ app/controllers/messeges_controller.rb
@@ -1,4 +1,23 @@
 class MessegesController < ApplicationController
+
   def index
+    @messege = Messege.new
+    @room = Room.find(params[:room_id])
+  end
+
+  def create
+    @room = Room.find(params[:room_id])
+    @messege = @room.messeges.new(message_params)
+    if @messege.save
+      redirect_to room_messeges_path(@room)
+    else
+      render :index
+    end
+  end
+
+  private
+
+  def message_params
+    params.require(:messege).permit(:content).merge(user_id: current_user.id)
   end
 end
